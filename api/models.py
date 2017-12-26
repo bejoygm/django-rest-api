0461545958fd44aad3833cfd2e7dcdc4bb350d51
Bejoy Mathew <bejoygm@gmail.com>
2017-12-26 08:11:58 +0530

    first test for models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class TeamMember(models.Model):
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', 
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+    email = models.CharField(max_length=255, blank=True)
+    # role
+    
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import TeamMember
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the teammember model."""
+
+    def setUp(self):
+        """Defines the test client and other test variables."""
+        self.teammember_first_name = "Alpha"
+        self.teammember = TeamMember(first_name=self.teammember_first_name)
+
+    def test_model_can_create_a_teammember(self):
+        old_count = TeamMember.objects.count()
+        self.teammember.save()
+        new_count = TeamMember.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
