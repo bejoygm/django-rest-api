01fde4875c11903f82e9150e5acdc511d492d065
Bejoy Mathew <bejoygm@gmail.com>
2017-12-26 19:42:58 +0530

    added api doc, updated requirements.txt

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.documentation import include_docs_urls
 from .views import CreateView, DetailsView
 
 urlpatterns = {
     path('team/', CreateView.as_view(), name="create"),
     path('team/<user_id>/', DetailsView.as_view(), name="details"),
+    path('docs/', include_docs_urls(title='Manage Team APIs'))
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
